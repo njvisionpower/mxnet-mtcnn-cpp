ae99821f8aa175bc9d1be340acf374b8e42200c0
zhizec <zhizec@163.com>
2019-01-23 14:02:18 +0800

    change main file

--- src/main.cpp
+++ src/main.cpp
@@ -25,9 +25,9 @@ void test_mtcnn()
 		std::cout << "face landmark: x0=" << face.landmark.x[0] << " x1=" << face.landmark.x[1] << "x2=" << face.landmark.x[2] << "x3=" << face.landmark.x[3] <<"x4="<<face.landmark.x[4]<< std::endl;
 		std::cout << "face landmark: y0=" << face.landmark.y[0] << " y1=" << face.landmark.y[1] << "y2=" << face.landmark.y[2] << "y3=" << face.landmark.y[3] << "y4=" << face.landmark.y[4] << std::endl;
 
-		for (int i = 0; i < 5; i++)
+		for (int j = 0; j < 5; j++)
 		{
-			cv::Point p(face.landmark.x[i], face.landmark.y[i]);
+			cv::Point p(face.landmark.x[j], face.landmark.y[j]);
 			cv::circle(img, p, 2, cv::Scalar(0, 0, 255), -1);
 		}
 
